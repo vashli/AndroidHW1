98fd806fd31635a3b5120498b38bb8cce1dd0a0d
vashli <mberi15@freeuni.edu.ge>
2019-03-28 15:54:41 +0400

    added rating functionality

--- app/src/main/java/com/vashli/playstoreproduct/MainActivity.java
+++ app/src/main/java/com/vashli/playstoreproduct/MainActivity.java
@@ -1,23 +1,26 @@
 package com.vashli.playstoreproduct;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private Button uninstallButton;
     private Button openButton;
     private ImageView travelIcon;
     private ImageView similarIcon;
-    private RecyclerView starRecyclerView;
+    private LinearLayout starsLayout;
 
+    private static final int NUM_STARS = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,13 +30,44 @@ public class MainActivity extends AppCompatActivity {
         travelIcon = findViewById(R.id.activity_main_image_view_travel);
         similarIcon = findViewById(R.id.activity_main_image_view_similars);
         addOnClickListeners();
-
-        starRecyclerView = findViewById(R.id.activity_main_recycler_view_stars);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        starRecyclerView.setLayoutManager(layoutManager);
-        starRecyclerView.setAdapter(new StarsAdapter());
+        initStars();
     }
 
+    private void initStars(){
+        starsLayout = findViewById(R.id.activity_main_layout_stars);
+        final ArrayList<ImageView> stars = new ArrayList<>();
+
+        for (int i = 0; i < NUM_STARS; i++){
+            ImageView star = new ImageView(this);
+            star.setImageDrawable(ContextCompat.getDrawable(star.getContext(), R.drawable.icons8_star_500));
+            starsLayout.addView(star);
+            final float scale = star.getContext().getResources().getDisplayMetrics().density;
+            int pixels = (int) (64 * scale + 0.5f);
+            star.getLayoutParams().height = pixels;
+            star.getLayoutParams().width = pixels;
+            star.setPadding(52,0,52,0);
+            star.requestLayout();
+            stars.add(star);
+        }
+
+        for (int i = 0; i < NUM_STARS; i++){
+            final int index = i;
+            stars.get(i).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    for(int j = 0; j <= index; j++){
+                        stars.get(j).setImageDrawable(ContextCompat.getDrawable(stars.get(j).getContext(), R.drawable.icons8_star_filled_500));
+                    }
+
+                    for(int j = index + 1; j < NUM_STARS; j++){
+                        stars.get(j).setImageDrawable(ContextCompat.getDrawable(stars.get(j).getContext(), R.drawable.icons8_star_500));
+                    }
+                }
+            });
+        }
+
+
+    }
     private  void addOnClickListeners(){
         uninstallButton.setOnClickListener(new View.OnClickListener() {
             @Override
